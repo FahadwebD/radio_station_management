a746782255735c6b4f7c09f26cdab8f830d1880b
FahadwebD <fahadchowdhury66779@gmail.com>
2022-05-20 15:09:04 +0600

    firebase setup for authentication

---
+++ src/components/Login/Firebase/firebase.config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgAt2lGc6NR9HbKVP0hRkI8yCTztk0eqI",
+  authDomain: "station-management-e839d.firebaseapp.com",
+  projectId: "station-management-e839d",
+  storageBucket: "station-management-e839d.appspot.com",
+  messagingSenderId: "201999041595",
+  appId: "1:201999041595:web:ea35f7a421988c850a881e"
+};
+
+export default firebaseConfig;
